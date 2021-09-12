f3d5dbce772929b18488b27bdcacb4b1928ffee5
lordgr8t <lordgr8t@gmail.com>
2021-09-13 01:31:18 +0300

    v1.0

---
+++ center_gr8t.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('#center-wrapper').css({'paddingTop' : '0.1px'});	
+	let	wrapWidth = $('#center-wrapper').width();
+	let	wrapHeight = $('#center-wrapper').height();
+	let contentWidthL = $('#center-width').width();
+	let contentHeightP = $('#center-heigh').height();
+	let contentWidthA = $('#center-all').width();
+	let contentHeightA = $('#center-all').height();
+	let	currentWidth = (wrapWidth/2)-(contentWidthL/2);
+	let	currentHeight = (wrapHeight/2)-(contentHeightP/2);
+	let	currentAllWidth = (wrapWidth/2)-(contentWidthA/2);
+	let	currentAllHeight = (wrapHeight/2)-(contentHeightA/2);
+	$('#center-wrapper #center-width').css({'marginLeft' : currentWidth});
+	$('#center-wrapper #center-heigh').css({'marginTop' : currentHeight});
+	$('#center-wrapper #center-all').css({'marginTop' : currentAllHeight}).css({'marginLeft' : currentAllWidth});
+});
