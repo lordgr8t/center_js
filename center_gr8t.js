286e38bc69722f22cc472ed93f4b334b0bc1b145
lordgr8t <57000268+lordgr8t@users.noreply.github.com>
2021-09-13 17:45:32 +0300

    Update center_gr8t.js

--- center_gr8t.js
+++ center_gr8t.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {	
 	$('*#center-wrapper').css({'paddingTop' : '0.1px'});	
-	let	wrapWidth = $('*#center-width').parent().width();
-	let	wrapHeight = $('*#center-heigh').parent().height();
+	let wrapWidth = $('*#center-width').parent().width();
+	let wrapHeight = $('*#center-heigh').parent().height();
 	let contentWidthL = $('#center-width').width();
 	let contentHeightP = $('#center-heigh').height();
 	let contentWidthA = $('#center-all').width();
 	let contentHeightA = $('#center-all').height();
-	let	currentWidth = (wrapWidth/2)-(contentWidthL/2);
-	let	currentHeight = (wrapHeight/2)-(contentHeightP/2);
-	let	currentAllWidth = (wrapWidth/2)-(contentWidthA/2);
-	let	currentAllHeight = (wrapHeight/2)-(contentHeightA/2);
+	let currentWidth = (wrapWidth/2)-(contentWidthL/2);
+	let currentHeight = (wrapHeight/2)-(contentHeightP/2);
+	let currentAllWidth = (wrapWidth/2)-(contentWidthA/2);
+	let currentAllHeight = (wrapHeight/2)-(contentHeightA/2);
 	$('*#center-wrapper #center-width').css({'marginLeft' : currentWidth});
 	$('*#center-wrapper #center-heigh').css({'marginTop' : currentHeight});
 	$('*#center-wrapper #center-all').css({'marginTop' : currentAllHeight}).css({'marginLeft' : currentAllWidth});
